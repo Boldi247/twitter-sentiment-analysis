6e9e6cd4abeeeafdc77de85b40581248c7fb6723
Boldi247 <kovacs.boldizsar0321@gmail.com>
2024-11-16 17:48:44 +0100

    typo fix, country -> origin

--- read_data.py
+++ read_data.py
@@ -8,7 +8,7 @@ def read_data():
     if training is None or test is None:
         raise Exception("Unable to read data")
 
-    new_columns = ["id", "country", "label", "text"]
+    new_columns = ["id", "origin", "label", "text"]
     training.columns = new_columns
 
     return training, test
